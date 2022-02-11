76b789911a58d8846aff76496db4f7731369eda3
EastBayEv <muzzall@stanford.edu>
2022-02-11 11:54:19 -0800

    Feb 2022

---
+++ _build/jupyter_execute/winter2022/Chapter 7.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Chapter 7 - Combine the basics in powerful ways!
+# 
+# Attend the below CIDR workshops and learn to put the basics together in more efficient and processual ways that will supercharge your text analysis and machine learning!
+
+# ![robot](img/robot.png)
+
+# [Raúl Arrabales Moreno - Conscious-Robots Reloaded!](https://www.conscious-robots.com/2016/01/05/conscious-robots-reloaded/)
+
+# ## CIDR Text Analysis with Python workshop
+# Learn methodical text analysis approaches to single documents and corpora with the [spaCy](https://spacy.io/usage/spacy-101) and [textacy](https://textacy.readthedocs.io/en/stable/api_reference/root.html) libraries: 
+# 
+# https://github.com/sul-cidr/Workshops/tree/master/Text_Analysis_with_Python
+# 
+# ## CIDR Intro to Machine Learning with Python workshop
+# Familiarize yourself with procedures for document and image classification - as well as other analyses - along with cloud-based resources for machine and deep learning with the [scikit-learn](https://scikit-learn.org/stable/tutorial/index.html), [lime](https://github.com/marcotcr/lime), and [tensorflow](https://www.tensorflow.org/guide/data) libraries: 
+# 
+# https://github.com/sul-cidr/Workshops/tree/master/Intro_to_ML_with_Python
