48c86e27f2c9f2a5297ab723fe724fb370b23373
EastBayEv <muzzall@stanford.edu>
2022-02-11 15:03:30 -0800

    Feb 2022

---
+++ _build/jupyter_execute/winter2022/start.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ii. Start coding!
+
+# ## Right click the "Launch Binder" button below and open it in a new browser tab. 
+# 
+# Follow these instructions in JupyterLab to start a kernel, type and run a line of code, save and export your work, and end your session. The content in JupyterLab and the JupyterBook is identical. 
+
+# [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/EastBayEv/SSDS-TAML/HEAD)
+
+# ## Click "Python 3 (ipykernel)" 
+# 
+# Click "Python 3 (ipykernel) under the "Notebook" header to launch a blank notebook. 
+# ![setkernel](img/kernel.png)
+
+# # Cells are code by default
+# 
+# In the first cell, type:
+#     
+# > print("Your Name Here") 
+
+# ## Run the code
+# 
+# Press `shift + enter` in JupyterLab or your local Python Anaconda installation to execute the code. 
+
+# ## Insert new cells
+# 
+# Click the "plus" button to insert a new cell. It can be found between the "save disk" and "scissors" icons. 
+# ![addcell](img/addcell.png)
+
+# ## Switch between code and text cells
+# 
+# Switch between code and text cells by clicking the dropdown menu. 
+# ![switch](img/switch.png)
+
+# ## Editing a markdown cell
+# 
+# Double-click a markdown cell to edit it. Press `shift + enter` to render the text. 
+# 
+# Go through the cells on this page and check out [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) to explore formatting rules and shortcuts. 
+# 
+# ![toc](img/toc.png)
+
+# ## Save your work
+# 
+# Click the "disk" icon to save your work. Right-click the tab to give your notebook a name:
+# 
+# ![rename](img/rename.png)
+
+# * Click "File" --> "Download" to save your Jupyter Notebook (extension `.ipynb`) before exiting a Jupyter Lab session. 
+# * There are many other options, take some time to explore the top menu and buttons.  
+# 
+# ![download](img/dl.png)
+
+# ## Close and halt
+# 
+# Safely close the notebook by clicking "File" --> "Close and Halt" before closing your browser.
+
+# ![closehalt](img/close.png)
+
+# Local Python installation
+# 
+# Despite the many cloud-based Python Jupyter Notebook solutions, it is always good to have a local installation on your own computer. Point your web browswer here to install Python Anaconda distribution 3.9 (as of January 2022): https://www.anaconda.com/products/individual
+# 
+# * Download the installation file for your operating system, open it, and follow the instructions to install. 
+# * Once installation completes, open the application named "Anaconda Navigator". It looks like this: 
+# 
+# ![navigator](img/navigator.png)
+# 
+# * Click the "Launch" button under either Jupyter Notebook or Jupyter Lab. 
+# * Open the `.ipynb` notebook you just downloaded from JupyterLab in your local installation and repeat the steps above.
+
+# ## Run these materials on your local computer
+# 
+# Copy these materials for use on your local installation. 
+# 
+# 1. Visit: https://github.com/EastBayEv/SSDS-TAML
+# 2. Click the green "Code" button
+# ![code](img/code.png)
+# 3. Click "Download ZIP"
+# ![zip](img/zip.png)
+# 4. Extract this folder someplace familiar such as your Desktop.
+# 5. Open Anaconda Navigator, launch JupyterLab, and navigate your directories to launch these notebooks.
+# 
+# ## git clone
+# Git users type: `git clone https://github.com/EastBayEv/SSDS-TAML.git`
+
+# ## Install external libraries
+# 
+# Install user-defined software libraries to enhance Python's functionality. In a new notebook cell type `!pip install <library name>`, e.g.: 
+# * `!pip install pandas` 
+# * `!pip install seaborn`
+
+# ## Dead kernel? 
+# 
+# The notebook kernel will fail from time to time, which is normal. Simply click "Kernel" from the File menu and one of the "Restart" options.
+
+# ![restart](img/restart.png)
+
+# > NOTE: about the terminal, you will see it run processes in the background like this:
+# 
+# ![terminal](img/terminal.png)
+
+# ## If you accidentally delete a Jupyter Notebook ...
+# 
+# Check your operating system's Recycle Bin!
---
+++ _build/jupyter_execute/winter2022/viz.py
@@ -0,0 +1,447 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # v. Visualization essentials
+
+# ![viz](img/viz.png)
+
+# In[1]:
+
+
+# import libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# make sure plots show in the notebook
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# After importing data, you should examine it closely. 
+# 
+# 1. Look at the raw data ans perform rough checks of your assumptions
+# 2. Compute summary statistics
+# 3. Produce visualizations to illustrate obvious - or not so obvious - trends in the data
+
+# ## Plotting with seaborn
+# 
+# ### First, a note about matplotlib
+# There are many different ways to visualize data in Python but they virtually all rely on matplotlib. You should take some time to read through the tutorial: https://matplotlib.org/stable/tutorials/introductory/pyplot.html. 
+# 
+# Because many other libraries depend on matplotlib under the hood, you should familiarize yourself with the basics. For example: 
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5]
+y = [2,4,6,8,20]
+plt.scatter(x, y)
+plt.title('title')
+plt.ylabel('some numbers')
+plt.xlabel('x-axis label')
+plt.show()
+
+
+# ## Visualization best practices
+# 
+# Consult Wilke's _Fundamentals of Data Visualization_ https://clauswilke.com/dataviz/ for discussions of theory and best practices. 
+# 
+# The **goal of data visualization** is to accurately communicate _something_ about the data. This could be an amount, a distribution, relationship, predictions, or the results of sorted data.
+# 
+# Utilize characteristics of different data types to manipulate the aesthetics of plot axes and coordinate systems, color scales and gradients, and formatting and arrangements to impress your audience!
+# 
+# ![wilke](img/wilke.png)
+
+# ![wilke12](img/wilke12.png)
+
+# ## Summary statistics - pandas review
+
+# In[3]:
+
+
+# load the Gapminder dataset
+gap = pd.read_csv("data/gapminder-FiveYearData.csv")
+
+
+# In[5]:
+
+
+# view column names of Gapminder data
+gap.columns
+
+
+# ### All columns
+
+# In[5]:
+
+
+# mean of all variables except country
+gap.groupby('continent').mean()
+
+
+# ### One column
+
+# In[7]:
+
+
+# Mean life expectancy for each continent
+gap.groupby('continent')["lifeExp"].mean()
+
+
+# ### Multiple columns
+
+# In[8]:
+
+
+# Mean lifeExp and gdpPercap for each continent
+le_table = gap.groupby('continent')[["lifeExp", "gdpPercap"]].mean()
+le_table
+
+
+# ## Basic plots
+# 
+# 1. Histogram: visualize distribution of one continuous (i.e., integer or float) variable.
+# 
+# 2. Boxplot: visualize the distribution of one continuous variable.
+# 
+# 3. Scatterplot: visualize the relationship between two continuous variables. 
+
+# ## Histogram
+# 
+# Use a histogram to plot the distribution of one continuous (i.e., integer or float) variable. 
+
+# In[22]:
+
+
+# all data
+sns.histplot(data = gap,
+            x = 'lifeExp'); 
+
+
+# In[13]:
+
+
+# by continent
+sns.histplot(data = gap, 
+            x = 'lifeExp', 
+            hue = 'continent');
+
+
+# ## Boxplot
+# 
+# Boxplots can be used to visualize one distribution as well, and illustrate different aspects of the table of summary statistics.
+
+# In[10]:
+
+
+# summary statistics
+gap.describe()
+
+
+# In[14]:
+
+
+# all data
+sns.boxplot(data = gap,
+            y = 'lifeExp', 
+            color = 'gray')
+
+
+# In[16]:
+
+
+gap.groupby('continent').count()
+
+
+# In[20]:
+
+
+# by continent
+sns.boxplot(data = gap,
+            x = 'continent', 
+            y = 'lifeExp').set_title('Boxplots');
+
+
+# In[34]:
+
+
+# custom colors
+sns.boxplot(data = gap, 
+            x = 'continent', 
+            y = 'lifeExp', 
+            palette = ['gray', '#8C1515', '#D2C295', '#00505C', 'white']).set_title('Boxplots');
+
+
+# ## Scatterplot
+# 
+# Scatterplots are useful to illustrate the relationship between two continuous variables. Below are several options for you to try.
+
+# In[28]:
+
+
+### change figure size
+sns.set(rc = {'figure.figsize':(12,8)})
+
+### change background
+sns.set_style("ticks")
+
+# commented code
+ex1 = sns.scatterplot(
+    
+    # dataset
+    data = gap,
+    
+    # x-axis variable to plot
+    x = 'lifeExp', 
+    
+    # y-axis variable to plot
+    y = 'gdpPercap', 
+    
+    # color points by categorical variable
+    hue = 'continent', 
+    
+    # point transparency
+    alpha = 1)
+
+### log scale y-axis
+ex1.set(yscale="log")
+
+### set axis labels
+ex1.set_xlabel("Life expectancy (Years)", fontsize = 20)
+ex1.set_ylabel("GDP per cap (US$)", fontsize = 20);
+
+### unhashtag to save 
+### NOTE: this might only work on local Python installation and not JupyterLab - try it!
+
+# plt.savefig('img/scatter_gap.pdf')
+
+
+# ## Quiz - Penguins dataset
+# 
+# Learn more about the biological and spatial characteristics of penguins! 
+# 
+# ![penguins](img/penguins.png)
+# 
+# 1. Use seaborn to make one of each of the plots in the image below. Check out the seaborn tutorial for more examples and formatting options: https://seaborn.pydata.org/tutorial/function_overview.html
+# 2. What might you conclude about the species of penguins from this dataset? 
+# 
+# ![sns](img/sea.png)
+# 
+# ### Map of Antarctica
+# 
+# Below is a map of Antarctica past the southernmost tip of the South American continent. 
+# 
+# The distance from the Biscoe Islands (Renaud) to the Torgersen and Dream Islands is about 140 km. 
+# 
+# ![antarctica](img/antarctica.png)
+
+# In[30]:
+
+
+# get help with the question mark
+# sns.scatterplot?
+
+
+# In[31]:
+
+
+# load penguins data
+penguins = pd.read_csv('data/penguins.csv')
+
+
+# In[32]:
+
+
+# hint: 
+penguins.groupby('island').count()
+
+
+# In[33]:
+
+
+# hint:
+penguins.groupby('island').mean()
+
+
+# In[19]:
+
+
+# 1. relational - scatterplot
+# your answer here:
+
+
+# In[20]:
+
+
+# 2. relational - lineplot
+# your answer here:
+
+
+# In[21]:
+
+
+# 3. distributions - histplot
+# your answer here:
+
+
+# In[22]:
+
+
+# 4. distributions - kdeplot
+# your answer here:
+
+
+# In[23]:
+
+
+# 5. distributions - ecdfplot
+# your answer here:
+
+
+# In[24]:
+
+
+# 6. distributions - rugplot
+# your answer here:
+
+
+# In[25]:
+
+
+# 7. categorical - stripplot
+# your answer here:
+
+
+# In[26]:
+
+
+# 8. categorical - swarmplot
+# your answer here:
+
+
+# In[27]:
+
+
+# 9. categorical - boxplot
+# your answer here:
+
+
+# In[28]:
+
+
+# 10. categorical - violinplot
+# your answer here:
+
+
+# In[29]:
+
+
+# 11. categorical - pointplot
+# your answer here:
+
+
+# In[30]:
+
+
+# 12. categorical - barplot
+# your answer here:
+
+
+# ## Quiz - Gapminder dataset
+# 
+# Make the twelve plots using the Gapminder dataset.
+# 
+# What can you conclude about income and life expectancy?
+# 
+# Visit https://www.gapminder.org/ to learn more!
+# 
+# ### Things you are probably wrong about!
+# 
+# ![gapm](img/gapm.png)
+# 
+# See the survey and correct response rate of the [Sustainable Development Misconception Study 2020](https://www.gapminder.org/ignorance/studies/sdg2020/)
+
+# In[31]:
+
+
+# 1. relational - scatterplot
+# your answer here:
+
+
+# In[32]:
+
+
+# 2. relational - lineplot
+# your answer here:
+
+
+# In[33]:
+
+
+# 3. distributions - histplot
+# your answer here:
+
+
+# In[34]:
+
+
+# 4. distributions - kdeplot
+# your answer here:
+
+
+# In[35]:
+
+
+# 5. distributions - ecdfplot
+# your answer here:
+
+
+# In[36]:
+
+
+# 6. distributions - rugplot
+# your answer here:
+
+
+# In[37]:
+
+
+# 7. categorical - stripplot
+# your answer here:
+
+
+# In[38]:
+
+
+# 8. categorical - swarmplot
+# your answer here:
+
+
+# In[39]:
+
+
+# 9. categorical - boxplot
+# your answer here:
+
+
+# In[40]:
+
+
+# 10. categorical - violinplot
+# your answer here:
+
+
+# In[41]:
+
+
+# 11. categorical - pointplot
+# your answer here:
+
+
+# In[42]:
+
+
+# 12. categorical - barplot
+# your answer here:
+
