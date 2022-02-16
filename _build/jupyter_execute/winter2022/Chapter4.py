c7d0c7b7a0df58b6f4754a76e4fc3cf2b4eab18d
EastBayEv <muzzall@stanford.edu>
2022-02-16 08:20:57 -0800

    Feb 2022

--- _build/jupyter_execute/winter2022/Chapter4.py
+++ _build/jupyter_execute/winter2022/Chapter4.py
@@ -78,3 +78,79 @@
 
 
 # graphviz installation instructions: https://graphviz.org/download/
+
+# ## Visualizations
+
+# ![accloss](img/accloss.png)
+
+# ![weboard](img/weboard.png)
+
+# ## Semantic similarity
+# 
+# See "Application Examples" section here: https://github.com/UKPLab/sentence-transformers
+
+# In[5]:
+
+
+from sentence_transformers import SentenceTransformer, util
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Two lists of sentences
+sentences1 = ['The cat sits outside',
+             'A man is playing guitar',
+             'The new movie is awesome']
+
+sentences2 = ['The dog plays in the garden',
+              'A woman watches TV',
+              'The new movie is so great']
+
+#Compute embedding for both lists
+embeddings1 = model.encode(sentences1, convert_to_tensor=True)
+embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+
+#Compute cosine-similarits
+cosine_scores = util.cos_sim(embeddings1, embeddings2)
+
+#Output the pairs with their score
+for i in range(len(sentences1)):
+    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
+
+
+# ### Find pairs of sentences with highest cosine similarity scores
+
+# In[6]:
+
+
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Single list of sentences
+sentences = ['The cat sits outside',
+             'A man is playing guitar',
+             'I love pasta',
+             'The new movie is awesome',
+             'The cat plays in the garden',
+             'A woman watches TV',
+             'The new movie is so great',
+             'Do you like pizza?']
+
+#Compute embeddings
+embeddings = model.encode(sentences, convert_to_tensor=True)
+
+#Compute cosine-similarities for each sentence with each other sentence
+cosine_scores = util.cos_sim(embeddings, embeddings)
+
+#Find the pairs with the highest cosine similarity scores
+pairs = []
+for i in range(len(cosine_scores)-1):
+    for j in range(i+1, len(cosine_scores)):
+        pairs.append({'index': [i, j], 'score': cosine_scores[i][j]})
+
+#Sort scores in decreasing order
+pairs = sorted(pairs, key=lambda x: x['score'], reverse=True)
+
+for pair in pairs[0:10]:
+    i, j = pair['index']
+    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences[i], sentences[j], pair['score']))
+
