3f341848b9c22239641defe589a4dfd65f363050
EastBayEv <muzzall@stanford.edu>
2022-02-23 13:18:31 -0800

    Feb 2022

--- _build/jupyter_execute/winter2022/Chapter5.py
+++ _build/jupyter_execute/winter2022/Chapter5.py
@@ -63,7 +63,7 @@ h2o.init()
 
 
 # Import a sample binary outcome train/test set into H2O
-# Learn about subset of Higgs Boson dataset: https://www.kaggle.com/c/higgs-boson
+# Learn about subset of Higgs Boson dataset: https://www.kaggle.com/c/higgs-boson/data
 train = h2o.import_file("https://s3.amazonaws.com/erin-data/higgs/higgs_train_10k.csv")
 test = h2o.import_file("https://s3.amazonaws.com/erin-data/higgs/higgs_test_5k.csv")
 
